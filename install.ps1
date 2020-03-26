3dc24ddccb4192212b872acd8a124dc99f644521
Mads Slotsbo <mads.slotsbo@gmail.com>
2020-03-26 08:10:58 +0100

    Merging work configs

--- install.ps1
+++ install.ps1
@@ -19,28 +19,25 @@ foreach ($folder in $containingFolders) {
     if (-not (Test-Path $filemapPath)) {
         continue;
     }
-    $localFilemap = Get-Content $filemapPath | ConvertFrom-Json -AsHashtable
+    $localFilemap = Get-Content $filemapPath | ConvertFrom-Json
 
     Write-Host $folder.FullName
-    foreach ($map in $localFilemap.GetEnumerator()) {
-        Write-Host $map.Key
-        Write-Host $map.Value
-        $fileLocation = Join-Path -Path $folder -ChildPath $map.Key
+    foreach ($map in $localFilemap.PSObject.Properties) {
+        $fileLocation = Join-Path -Path $folder -ChildPath $map.Name
         $filemap[$fileLocation] = $map.Value
     }
 }
 
 Write-Host "Filemaps:"
-$filemap | ForEach-Object { [PSCustomObject]$_ } | Format-List
+$filemap | Format-Table
 
 foreach ($map in $filemap.GetEnumerator()) {
-
     if ((Test-Path $map.Value) -and -not $overwrite) {
         Write-Host "$($map.Value) already exists. Skipping because of overwite setting"
         continue;
     }
 
-    New-Item -ItemType SymbolicLink -Path $map.Value -Value $map.Key -Force:$Overwrite
+    New-Item -ItemType SymbolicLink -Path $map.Value -Value $map.Name -Force:$Overwrite
 }
 
 Pop-Location
