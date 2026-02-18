a1b834c4fa39293891d63320a2a46c3c6ccf6b87
Mads Slotsbo <mads.slotsbo@gmail.com>
2026-02-18 17:38:13 +0100

    Make git not touch line endings and linux skip windows profile stuff

--- PowerShell/Shared.Powershell_profile.ps1
+++ PowerShell/Shared.Powershell_profile.ps1
@@ -16,19 +16,20 @@ else {
     Write-Verbose "oh-my-posh not imported"
 }
 
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-    Import-Module "$ChocolateyProfile"
+if ($IsWindows) {
+    $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+    if (Test-Path($ChocolateyProfile)) {
+        Import-Module "$ChocolateyProfile"
+    }
+
+    $completionScripts = Get-ChildItem -Path "C:/completions/" -Filter *.ps1 -ErrorAction SilentlyContinue
+    foreach($script in $completionScripts) {
+        Write-Verbose "Loading completion: $script"
+        . $script
+    }
 }
 
 Import-Module "$PSScriptRoot/Utility.psm1"
-
-$completionScripts = Get-ChildItem -Path "C:/completions/" -Filter *.ps1  -ErrorAction SilentlyContinue
-
-foreach($script in $completionScripts) {
-    Write-Verbose "Loading completion: $script"
-    . $script
-}
 
 if (Test-Path("~/Powershell_Profile.ps1")) {
     Invoke-Expression -Command "~/Powershell_Profile.ps1"
